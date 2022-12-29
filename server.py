dcbebed069ed65c772e8582e1cd081f8c038502c
khabar0val <alexander.khabarov.123@gmail.com>
2022-12-29 16:06:32 +0300

    add register functional to debug

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+from src.models.auth import sign_up
+
+app = Flask(__name__)
+
+@app,route('/signup', methods = ['POST'])
+def signup(username, password):
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        sign_up(username, password)
+
+    else:
+        return render_template('signup.html', username=username, password=password)
+
+if __name__ == '__main__':
+    app.run()
